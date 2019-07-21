f1435c5366c4f2899de645360e70d3d2c28477cd
Bill McKinnon <bmamckinnon@gmail.com>
2019-07-21 18:49:37 -0400

    Show submitted activities. Show balance.

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,6 +2,8 @@ class ActivitiesController < ApplicationController
   include SessionsHelper
 
   def index
+    @activities = current_user.activities
+    @activity_balance = find_activity_balance.to_i
   end
 
   def new
@@ -11,7 +13,7 @@ class ActivitiesController < ApplicationController
   def create
     @activity = current_user.activities.build(activity_params)
     if @activity.save
-      flash.now[:success] = "Activity saved."
+      flash[:success] = "Activity saved."
       redirect_to activities_path
     else
       flash.now[:danger] = "There are errors."
@@ -30,5 +32,11 @@ class ActivitiesController < ApplicationController
   private
     def activity_params
       params.require(:activity).permit(:name, :category, :minutes)
-    end 
+    end
+
+    def find_activity_balance
+      contributor_sum = current_user.activities.where("category = 'contributor'").sum(:minutes)
+      subtractor_sum = current_user.activities.where("category = 'subtractor'").sum(:minutes)
+      contributor_sum - subtractor_sum
+    end
 end
