1e0b3a487157aa3d0e6c7ff704eebdc15b8993ef
Bill McKinnon <bmamckinnon@gmail.com>
2019-09-15 18:21:18 -0400

    Remove render sessions#new from sessions controller to avoid app crash.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,4 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -16,8 +13,8 @@ class SessionsController < ApplicationController
         redirect_to root_url
       end
     else
-      flash.now[:danger] = "Invalid email or password."
-      render 'new'
+      flash[:danger] = "Invalid email or password."
+      redirect_to root_url
     end
   end
 
