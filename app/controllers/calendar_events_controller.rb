0bb3fdaa46dabfa52e69d07ec5b6e8804c18e983
Bill McKinnon <bmamckinnon@gmail.com>
2019-10-28 03:17:19 -0400

    Integrate google calendar feature, add ability to pull calendar events and durations for the user to select in the view.

--- app/controllers/calendar_events_controller.rb
+++ app/controllers/calendar_events_controller.rb
@@ -5,13 +5,17 @@ class CalendarEventsController < ApplicationController
 
     service = Google::Apis::CalendarV3::CalendarService.new
     service.authorization = client
-    
+
     calendar_ids = service.list_calendar_lists.items.map { |calendar| calendar.id }
-    @events = calendar_ids
-      .map { |id| service.list_events(id, time_min: start_time, time_max: end_time) }
+
+    events = calendar_ids
+      .map { |id| service.list_events(id, time_min: convert_params(params[:start]), time_max: convert_params(params[:end])) }
       .map { |event| event.items }
       .flat_map { |item| item }
-      .map { |name| name.summary }
+    
+    @events_with_times = events
+      .reject { |event_resource| event_resource.start.nil? || event_resource.end.nil? }
+      .reject { |event_time| event_time.start.date_time.nil? || event_time.end.date_time.nil? }
 
   rescue Google::Apis::AuthorizationError
     response = client.refresh!
@@ -29,13 +33,14 @@ class CalendarEventsController < ApplicationController
       scope: Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY,
       redirect_uri: callback_url
     }
-  end    
-
-  def start_time
-    DateTime.strptime(params[:start].values.join, '%m%d%Y%H%M').rfc3339
   end
 
-  def end_time
-    DateTime.strptime(params[:end].values.join, '%m%d%Y%H%M').rfc3339
+  # Ensures params are ordered correctly, adds zero-padding to any values less
+  # than 10 and then produces the required RFC3339 timestamp.
+  def convert_params(original_params)
+    ordered_params = [:month, :day, :year, :hour, :minute].map { |date_part| original_params[date_part] }
+    converted_params = ordered_params.map { |value| sprintf('%02d', value.to_i) }
+    DateTime.strptime(converted_params.join, '%m%d%Y%H%M').rfc3339
   end
 end
+
