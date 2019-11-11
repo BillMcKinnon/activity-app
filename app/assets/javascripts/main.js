9176662f861ae57327936a99c3d37988893d2448
Bill McKinnon <bmamckinnon@gmail.com>
2019-11-11 16:47:41 -0500

    Implement data attributes into jquery code instead of using classes.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,17 +1,17 @@
 var ready = function() {
   //*******Dashboard*******
   //Balance will display red if value is negative or green otherwise.
-  var value = $(".balance").text();
+  var value = $("[data-balance]").text();
 
   if (value.indexOf("-") >= 0) {
-    $(".balance").addClass("red");
+    $("[data-balance]").addClass("red");
   } else {
-    $(".balance").addClass("green");
+    $("[data-balance]").addClass("green");
   }
 
   //When "Add new" is clicked within the existing activity dropdown (which is the only option with a blank value),
   //spawn the add new activity name and category inputs. Otherwise, hide the activity name and category inputs.
-  $("#entry_activity_id").change(function(){
+  $("[data-activity-name-select]").change(function(){
     if($(this).val() == "") {
       $(".activity-name-input, .activity-category-input").removeClass("hidden");
     } else {
@@ -19,7 +19,7 @@ var ready = function() {
     }
   });  
 
-  //*******Calendar events*******
+  //*******Calendar Events*******
   // When button is clicked, pull data from button and populate the form.
   $("[data-event-name]").click(function(e) {
     let eventName = $(this).data("eventName");
@@ -29,11 +29,10 @@ var ready = function() {
   });
 
   // When button is clicked, clear the form.
-  $("#clear_form").click(function(e) {
+  $("[data-clear-form]").click(function(e) {
     $("#activity_name, #activity_category, #entry_minutes").val("");
   });
 };
 
 $(document).ready(ready);
-$(document).on('turbolinks:load', ready);
 
