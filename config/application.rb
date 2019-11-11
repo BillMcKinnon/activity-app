15811622ea2e1f9a3a9c6fa20fafd8f15cbed0be
Bill McKinnon <bmamckinnon@gmail.com>
2019-11-11 12:51:27 -0500

    Change initialize on precompile to false to appease Heroku.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module ActivityApp
     config.load_defaults 5.2
     config.time_zone = 'Eastern Time (US & Canada)'
     config.active_record.default_timezone = :local
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
