72553a07948779f70c5be08b4357fb247d3734a1
Bill McKinnon <bmamckinnon@gmail.com>
2020-04-15 19:48:01 -0400

    Add 30 day view option for activity, add responsive styles for tables and activity page, populate tables with correct data

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,7 @@ class ActivitiesController < ApplicationController
   def show
     @activity = current_user.activities.find(params[:id])
     @past_week_activity_data = past_week_activity_data
+    @past_mont_activity_data = past_month_activity_data
   end
 
   private
@@ -13,6 +14,15 @@ class ActivitiesController < ApplicationController
       {
         name: @activity.name.titleize,
         data: past_data(@activity.name, "activities.name", 7)
+      }
+    ] 
+  end
+
+  def past_month_activity_data
+    [
+      {
+        name: @activity.name.titleize,
+        data: past_data(@activity.name, "activities.name", 30)
       }
     ] 
   end
